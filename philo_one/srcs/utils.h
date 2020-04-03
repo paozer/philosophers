b186387a411e3bc6ca5974139a490b910f489965
paozer <p.ramellaratin@gmail.com>
2020-04-03 11:33:39 +0200

    refactored utilities

--- philo_one/srcs/utils.h
+++ philo_one/srcs/utils.h
@@ -1,8 +1,7 @@
 #ifndef UTILS_H
 # define UTILS_H
 
-unsigned long	ft_atoul(const char *str);
-unsigned long	get_timestamp(void);
+unsigned long	ft_atol(const char *str);
 int				ft_atoi(const char *str);
 size_t			ft_strlen(const char *s);
 void			ft_putnbr(unsigned long n);
